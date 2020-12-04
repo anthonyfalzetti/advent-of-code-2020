6da9309e3646a64c92de1036fad679d75b044554
Anthony Falzetti <anthony.falzetti@gmail.com>
2020-12-04 06:51:22 -0700

    Part2

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,11 +1,25 @@
 defmodule AdventOfCode.Day03 do
   def part1(_args) do
-    parse_input()
-    |> traverse_path()
-    |> count_trees()
+    [{3,1}]
+    |> Enum.map(fn{right, down} ->
+      parse_input()
+      |> traverse_path(right, down)
+      |> count_trees()
+    end)
+    |> Enum.sum()
   end
 
   def part2(_args) do
+    [{1,1},{3,1},{5,1},{7,1},{1,2}]
+    # [{3,1}]
+    |> Enum.map(fn{right, down} ->
+      parse_input()
+      |> traverse_path(right, down)
+      |> count_trees()
+    end)
+    |> Enum.reduce(1, fn(count, acc) ->
+      count * acc
+    end)
   end
 
   defp parse_input() do
@@ -16,22 +30,26 @@ defmodule AdventOfCode.Day03 do
       |> String.trim()
       |> String.split("")
       |> Enum.reject(&(&1 == ""))
-      |> List.duplicate(50)
+      |> List.duplicate(1000)
       |> List.flatten()
     end)
   end
 
-  defp traverse_path(grid) do
+  defp traverse_path(grid, right, 2) do
+    [nil|grid]
+    |> Enum.drop_every(2)
+    |> traverse_path(right, 1)
+  end
+
+  defp traverse_path(grid, right, _down) do
     grid
-    |> Enum.reduce({-3, []}, fn(row, {starting_location, path}) ->
-      current_location = starting_location + 3
+    |> Enum.reduce({-right, []}, fn(row, {starting_location, path}) ->
+      current_location = starting_location + right
       object = Enum.at(row, current_location)
-      |> IO.inspect(label: :object)
 
       {current_location, [object|path]}
     end)
     |> elem(1)
-    |> IO.inspect(label: :objects)
   end
 
   defp count_trees(list_of_objects) do
