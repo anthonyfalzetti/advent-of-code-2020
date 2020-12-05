a60382d6c4355ad1cc464b4204b7a63d0e2bc4de
Anthony Falzetti <anthony.falzetti@gmail.com>
2020-12-05 06:54:45 -0700

    Day 4 part 1

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,40 @@
 defmodule AdventOfCode.Day04 do
   def part1(_args) do
+    parse_input()
+    |> filter_rows()
+    |> Enum.count()
   end
 
   def part2(_args) do
+  end
+
+  defp parse_input() do
+    "data/day_4_input"
+    |> File.read!()
+    |> String.split("\n\n")
+    |> Enum.map(fn(row) ->
+      row
+      |> String.replace("\n", " ")
+      |> String.split(" ")
+      |> Enum.reduce(%{}, fn(kv_pairs, acc) ->
+        [k, v] = String.split(kv_pairs, ":")
+        Map.put(acc, k, v)
+      end)
+    end)
+  end
+
+  defp filter_rows(rows_list) do
+    rows_list
+    |> Enum.filter(&validate_row(&1))
+  end
+
+
+  defp validate_row(row) do
+    ["byr", "ecl", "eyr", "hcl", "hgt", "iyr", "pid"]
+    |> Enum.all?(fn(k) ->
+      row
+      |> Map.keys()
+      |> Enum.member?(k)
+    end)
   end
 end
